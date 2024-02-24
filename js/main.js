46f28b629d60f7f73844c078be193e3b3823aaa3
Richard Jackson <richard.jackson@nightingalehq.ai>
2024-02-24 00:34:05 +0000

    Iframes centered, plus some JS

--- js/main.js
+++ js/main.js
@@ -2,3 +2,21 @@
 
 // This file is intentionally left blank
 document.getElementById('copyright-year').innerHTML = new Date().getFullYear();
+
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+     console.log(entry);
+    if(entry.isIntersecting) {
+      entry.target.classList.add('show');
+      
+    } else {
+        entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach(element => {
+  observer.observe(element);
+});
